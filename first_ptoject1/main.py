6e106dfc927d94a89b10355c0498c3ad89814efd
Alexander <orloff.alek5@yandex.ru>
2020-10-31 13:05:47 +0300

    first commit

---
+++ first_ptoject1/main.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+
+size = width, height = 800, 600
+black = 0, 0, 0
+
+green = (0, 255, 0)
+
+thickness = 5
+
+ppi = 3.1415
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+        screen.fill(black)
+
+        pygame.draw.circle(screen, green, (400, 300), 250, thickness)
+        pygame.draw.circle(screen, green, (300, 200), 30, thickness)
+        pygame.draw.circle(screen, green, (500, 200), 30, thickness)
+
+        pygame.draw.line(screen, green, (400, 250), (400, 380), thickness)
+
+        r = pygame.Rect(240, 230, 320, 250)
+        pygame.draw.arc(screen, green, r, 5 / 4 * ppi, 7 / 4 * ppi, thickness)
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+    sys.exit
+    
+if __name__ == '__main__':
+    main()
